9cb20b1344302ddeb5a1d47e330f6b5edec7de84
Nazish <mehakstudio26@gmail.com>
2024-09-02 06:21:52 +0500

    PRODIGY_WD_1

---
+++ index.js
@@ -0,0 +1,11 @@
+// script.js
+
+// Change navbar background color on scroll
+window.addEventListener('scroll', function() {
+    var navbar = document.getElementById('navbar');
+    if (window.scrollY > 50) {
+        navbar.classList.add('scrolled');
+    } else {
+        navbar.classList.remove('scrolled');
+    }
+});
